26b00acd0a3dbe9da1b4ed9c55cd2054e774a9d6
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-20 20:35:27 +0700

    Greeting component

---
+++ src/components/greeting/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+const Greeting = () => {
+	return (
+		<div className="greeting-wrapper">
+			<h1>Hi, I'm Tins Pham Van - aka Tins</h1>
+		</div>
+	);
+};
+
+export default Greeting;
