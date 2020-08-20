ba6e402382da893afb04c9b3747cd3eb768f8e01
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-20 21:03:41 +0700

    Style for left-column

--- src/components/intro/index.jsx
+++ src/components/intro/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import Avatar from "../../assets/img/avatar.png";
 const Greeting = () => {
 	return (
 		<div className="intro-wrapper">
@@ -15,7 +16,28 @@ const Greeting = () => {
 					</li>
 				</ul>
 			</div>
-			<div className="left-column">left-column</div>
+			<div className="left-column">
+				<img id="avatar" src={Avatar} alt="Avatar" />
+				<h5
+					style={{
+						textAlign: "center",
+						lineHeight: 0,
+					}}
+				>
+					Personalize Theme
+				</h5>
+
+				<div id="theme-options-wrapper">
+					<div id="light-mode" className="theme-dot"></div>
+					<div id="blue-mode" className="theme-dot"></div>
+					<div id="green-mode" className="theme-dot"></div>
+					<div id="purple-mode" className="theme-dot"></div>
+				</div>
+
+				<p id="settings-note">
+					*Theme settings will be saved for your next vist
+				</p>
+			</div>
 			<div className="right-column">right-column</div>
 		</div>
 	);
