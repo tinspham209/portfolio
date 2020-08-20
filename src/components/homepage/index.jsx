7596f2504924783fb58f27f1035de70bdb504e2c
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-20 20:35:40 +0700

    Refactor with split component

--- src/components/homepage/index.jsx
+++ src/components/homepage/index.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import "./style.css";
+import Greeting from "../greeting";
+import Intro from "../intro";
 
 const Homepage = () => {
 	return (
 		<React.Fragment>
 			<section className="s1">
 				<div className="main-container">
-					<div className="greeting-wrapper">
-						<h1>Hi, I'm Tins Pham Van - aka Tins</h1>
-					</div>
-					<div className="intro-wrapper">
-						<div className="nav-wrapper">Nav</div>
-						<div className="left-column">left-column</div>
-						<div className="right-column">right-column</div>
-					</div>
+					<Greeting />
+					<Intro />
 				</div>
 			</section>
 			<section className="s2">
