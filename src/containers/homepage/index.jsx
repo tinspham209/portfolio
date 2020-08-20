e5f38e7fdec62cb0d394ebd801b09464ebe0084b
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-20 20:30:57 +0700

    homepage container

---
+++ src/containers/homepage/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import HomePageComponent from "../../components/homepage";
+const HomePage = () => {
+	return (
+		<div>
+			<HomePageComponent />
+		</div>
+	);
+};
+
+export default HomePage;
