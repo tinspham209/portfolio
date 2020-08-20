6a7a124de736baddca19b452e9d2ca2bc420d950
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-20 20:47:34 +0700

    Navigation

--- src/components/intro/index.jsx
+++ src/components/intro/index.jsx
@@ -3,7 +3,18 @@ import "./style.css";
 const Greeting = () => {
 	return (
 		<div className="intro-wrapper">
-			<div className="nav-wrapper">Nav</div>
+			<div className="nav-wrapper">
+				<div className="dots-wrapper">
+					<div id="dot-1" className="browser-dot"></div>
+					<div id="dot-2" className="browser-dot"></div>
+					<div id="dot-3" className="browser-dot"></div>
+				</div>
+				<ul id="navigation">
+					<li>
+						<a href="#">Contact</a>
+					</li>
+				</ul>
+			</div>
 			<div className="left-column">left-column</div>
 			<div className="right-column">right-column</div>
 		</div>
