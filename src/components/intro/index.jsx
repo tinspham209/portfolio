639860f25f3c65c6c59102519fc070b9de9380dd
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-20 20:35:18 +0700

    intro component

---
+++ src/components/intro/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+const Greeting = () => {
+	return (
+		<div className="intro-wrapper">
+			<div className="nav-wrapper">Nav</div>
+			<div className="left-column">left-column</div>
+			<div className="right-column">right-column</div>
+		</div>
+	);
+};
+
+export default Greeting;
