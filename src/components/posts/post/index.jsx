6c782947d85fd5073719d6992e955b2b528f8a6e
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-21 19:10:17 +0700

    Update style for post component

--- src/components/posts/post/index.jsx
+++ src/components/posts/post/index.jsx
@@ -7,7 +7,7 @@ const Post = ({ id, imgSrc, imgAlt, title, description, link }) => {
 				<img className="post__thumbnail" src={imgSrc} alt={imgAlt} />
 				<div className="post__preview">
 					<h6 className="post__title">{title}</h6>
-					<p className="post__intro">{description}</p>
+					<p className="post__description">{description}</p>
 					<a href={link}>Read More</a>
 				</div>
 			</div>
