11c16db5d86deec80b17f6c2abac5f2c90620d1d
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-21 18:33:57 +0700

    Refactor className with BEM

--- src/components/intro/index.jsx
+++ src/components/intro/index.jsx
@@ -5,48 +5,34 @@ const Greeting = () => {
 	return (
 		<div className="intro-wrapper">
 			<div className="nav-wrapper">
-				<div className="dots-wrapper">
-					<div id="dot-1" className="browser-dot"></div>
-					<div id="dot-2" className="browser-dot"></div>
-					<div id="dot-3" className="browser-dot"></div>
+				<div className="nav-wrapper-left__dots-wrapper">
+					<div className="browser-dot dot-1"></div>
+					<div className="browser-dot dot-2"></div>
+					<div className="browser-dot dot-3"></div>
 				</div>
-				<ul id="navigation">
+				<ul className="nav-wrapper-right__navigation-list">
 					<li>
 						<a href="#contact-form">Contact</a>
 					</li>
 				</ul>
 			</div>
 			<div className="left-column">
-				<img id="avatar" src={Avatar} alt="Avatar" />
-				<h5
-					style={{
-						textAlign: "center",
-						lineHeight: 0,
-					}}
-				>
-					Pham Van Tin
-				</h5>
-				<p
-					style={{
-						textAlign: "center",
-						lineHeight: 0,
-					}}
-				>
-					{" <@tinspham209> "}
-				</p>
+				<img className="avatar" src={Avatar} alt="Avatar" />
+				<h5>Pham Van Tin</h5>
+				<p>{" <@tinspham209> "}</p>
 			</div>
 			<div className="right-column">
-				<div id="preview-shadow">
-					<div id="preview">
-						<div id="corner-tl" className="corner"></div>
-						<div id="corner-tr" className="corner"></div>
+				<div className="right-column__preview-shadow">
+					<div className="right-column__preview">
+						<div className="corner corner-tl"></div>
+						<div className="corner corner-tr"></div>
 						<h3>What I Do?</h3>
 						<p>
 							A fresher developer using React for Web development, or Express
 							for Web services
 						</p>
-						<div id="corner-br" className="corner"></div>
-						<div id="corner-bl" className="corner"></div>
+						<div className="corner corner-br"></div>
+						<div className="corner corner-bl"></div>
 					</div>
 				</div>
 			</div>
